9dfe8c78026a629df511ac06a6b6bb70d3685478
yaxin <liuyaxin991109@gmail.com>
2023-07-21 22:36:29 +0800

    Update app.config.js

--- task_management/backend/config/app.config.js
+++ task_management/backend/config/app.config.js
@@ -1,6 +1,6 @@
 const MONGO_DB_CONFIG = {
     DB:
-        "mongodb+srv://liuyaxin:<pw>@cluster0.nz8d2r7.mongodb.net/tasks?retryWrites=true&w=majority"
+        "mongodb+srv://liuyaxin@cluster0.nz8d2r7.mongodb.net/tasks?retryWrites=true&w=majority"
 };
 
 module.exports = {
